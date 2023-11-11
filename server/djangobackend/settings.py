2242d0851b8f14bc2aea2306dc106badd0e052f8
BeBono <gitbebono@gmail.com>
2023-11-10 23:06:16 -0500

    Authentication, CarMake & CarModel models ok, running from mmy computer (off course: No include load of requireentes packages)

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -77,23 +77,23 @@ WSGI_APPLICATION = 'djangobackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'postgres',
-#         'USER': 'postgres',
-#         'PASSWORD': 'MTI3NTItYWxiZXJ0',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Perseverar',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
 
 
 # INSTALL_APP = {
